6a4b558215fdf3a874c70355cf20fc53f4414ced
mypasswords <42649310+MPPMC@users.noreply.github.com>
2018-10-09 11:00:01 +0200

    Initial Commit

---
+++ regis/authorization.php
@@ -0,0 +1,60 @@
+﻿
+<div style="width:85%;
+            padding:20px;
+            background:#E9E9E9;
+            border:#B1B1B1 2px outset;
+            -moz-border-radius:10px 10px 10px 10px;
+            -webkit-border-radius:10px 10px 10px 10px;
+            -khtml-border-radius:10px 10px 10px 10px;
+            border-radius:10px 10px 10px 10px;
+            behavior:url(border-radius.htc);">
+
+  <form action="handler.php" method="post">
+    <p style="margin-bottom:5px;">
+      <input style="width:90%;
+                    height:30px;
+                    padding:5px;
+                    font-size:24px;
+                    border:#999 1px solid;
+                    -moz-border-radius:5px 5px 5px 5px;
+                    -webkit-border-radius:5px 5px 5px 5px;
+                    -khtml-border-radius:5px 5px 5px 5px;
+                    border-radius:5px 5px 5px 5px;
+                    behavior:url(border-radius.htc);" name="login" type="name" placeholder="Login">
+    </p>
+    <p style="margin-bottom:5px;">
+      <input style="width:90%;
+                    height:30px;
+                    padding:5px;
+                    font-size:24px;
+                    border:#999 1px solid;
+                    -moz-border-radius:5px 5px 5px 5px;
+                    -webkit-border-radius:5px 5px 5px 5px;
+                    -khtml-border-radius:5px 5px 5px 5px;
+                    border-radius:5px 5px 5px 5px;
+                    behavior:url(border-radius.htc);" name="password" type="password" placeholder="Пароль">
+    </p>
+    <p style="margin-top:10px;">
+      <button style="width:auto;
+                     padding:5px 15px 5px 15px;
+                     margin-left:50px;
+                     -moz-appearance:none;
+                     -webkit-appearance:none;
+                     -ms-appearance:none;
+                     appearance:none;
+                     background-color:#FFF;
+                     color:#666 !important;
+                     cursor:pointer;
+                     display:inline-block;
+                     font-size:24px;
+                     text-align:center;
+                     text-decoration:none;
+                     border:#999 1px solid;
+                     -moz-border-radius:5px 5px 5px 5px;
+                     -webkit-border-radius:5px 5px 5px 5px;
+                     -khtml-border-radius:5px 5px 5px 5px;
+                     border-radius:5px 5px 5px 5px;
+                     behavior:url(border-radius.htc);" name="avto_ok" type="submit">Войти</button>
+    </p>
+  </form>
+</div>
